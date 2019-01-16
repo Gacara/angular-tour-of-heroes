a9fb600364600760e655e6cf3e8b0499b322832b
Romain Goulet <romain.goulet@devinci.fr>
2019-01-16 11:50:03 +0100

    Tour des héros
    Premier jet

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', axe: 'WEB', moyenne: 17 },
+  { id: 12, name: 'Narco', axe: 'WEB', moyenne: 16 },
+  { id: 13, name: 'Bombasto', axe: 'WEB', moyenne: 15 },
+  { id: 14, name: 'Celeritas', axe: 'WEB', moyenne: 14 },
+  { id: 15, name: 'Magneta', axe: 'WEB', moyenne: 12 },
+  { id: 16, name: 'RubberMan', axe: 'WEB', moyenne: 11 },
+  { id: 17, name: 'Dynama', axe: 'WEB', moyenne: 10 },
+  { id: 18, name: 'Dr IQ', axe: 'WEB', moyenne: 8 },
+  { id: 19, name: 'Magma', axe: 'WEB', moyenne: 6 },
+  { id: 20, name: 'Tornado', axe: 'WEB', moyenne: 5 }
+];
